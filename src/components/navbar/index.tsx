eade794d48631e7b24e65120cf8bb8c913e8c509
heliossandro <zenosama892@gmail.com>
2025-08-28 23:42:38 +0100

    tailwind

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,9 @@
-import "../../styles/style_navbar.css";
-
 export const Navbar = () => {
   return (
-    <ul className="list">
-      <li className="item"><a href="">Home</a></li>
-      <li className="item"><a href="">Curso</a></li>
-      <li className="item"><a href="">Sobre</a></li>
+    <ul className="flex gap-8">
+      <li className="hover:text-[#f8efe6] hover:bg-amber-50/30 w-15 h-7 rounded-sm flex justify-center text-center transition"><a href="">Home</a></li>
+      <li className="hover:text-[#f8efe6] hover:bg-amber-50/30 w-15 h-7 rounded-sm flex justify-center text-center transition"><a href="">Curso</a></li>
+      <li className="hover:text-[#f8efe6] hover:bg-amber-50/30 w-15 h-7 rounded-sm flex justify-center text-center transition"><a href="">Sobre</a></li>
     </ul>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,17 @@
-import "../../styles/style_home.css"
-import benef_form_img from "../../assets/images/beneficios_formacao_img.jpg"
+import location from"../../assets/icons/location.png"
 
 export const Home = () => {
   return (
     <>
-      <section className="section_title">
-        <h1 className="title"> Quer aprender a fazer Crochê?</h1>
-          <p className="text_title">Desperte a sua criatividade e domine a arte do croché com uma formação pensada especialmente para si! Em apenas duas semanas, vai aprender desde os pontos mais básicos até a criação de peças incríveis, como a elegante peça square. Além disso, terá acesso a bónus exclusivos que vão ajudar a transformar o seu talento numa oportunidade de negócio. As vagas são limitadas  não perca a chance de criar, empreender e dar vida às suas ideias com as próprias mãos!</p>
+      <section className="flex justify-center items-center flex-col p-20">
+        <h1 className="text-[#B27738] p-8 text-5xl"> Quer aprender a fazer Crochê?</h1>
+          <p className="w-350 text-base">Desperte a sua criatividade e domine a arte do croché com uma formação pensada especialmente para si! Em apenas duas semanas, vai aprender desde os pontos mais básicos até a criação de peças incríveis, como a elegante peça square. Além disso, terá acesso a bónus exclusivos que vão ajudar a transformar o seu talento numa oportunidade de negócio. As vagas são limitadas  não perca a chance de criar, empreender e dar vida às suas ideias com as próprias mãos!</p>
       </section>
 
-      <section className="section_info_course">
-        <h1 className="title"> Informações do curso</h1>
-          <div className="info_1">
-            <img src={benef_form_img} alt="" />
+      <section className="bg-gradient-to-b from-[#DFA464] to-[#B27738] ">
+        <h1 className="text-[#ffff] p-8 text-5xl flex justify-center"> Informações do curso</h1>
+          <div className="bg-amber-50  m-8 w-85 h-80">
+            <img src={location} alt="location" className="m-23 w-20 h-20 text-[#B27738]" />
           </div>
       </section>
     </>
