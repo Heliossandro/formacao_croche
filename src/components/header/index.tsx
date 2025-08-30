6e4d9dfcb4915efece09dc2d360f90631e536ed0
heliossandro <zenosama892@gmail.com>
2025-08-30 04:46:50 +0100

    50%

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import logo from "../../assets/images/logo.png"
 export const Header = () => {
   return (
     <div className="text-white">
-      <div className="bg-[#9F5C28] flex items-center justify-between py-6 ">
+      <div className="bg-[#9F5C28] flex items-center justify-between py-7 ">
         <div className="w-[80%] mx-auto flex items-center justify-between ">
           <div>logo</div>
            {/* <img src={logo} alt="" /> */}
