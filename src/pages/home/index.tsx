f91024e990ff29509a626f51030340106d492533
heliossandro <zenosama892@gmail.com>
2025-08-31 03:34:21 +0100

    66%

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,70 +2,71 @@ import location from"../../assets/icons/location.png"
 import money from"../../assets/icons/money.png"
 import clock from "../../assets/icons/clock.png"
 import calend from "../../assets/icons/calend.png"
+import VB_croche from "../../assets/images/vestidoeBlusa_croche.png"
 
 export const Home = () => {
   return (
     <>
       <section className="flex flex-col justify-center items-center p-10 md:p-20 min-h-[700px] gap-10 text-center">
-  <h1 className="text-[#B27738] text-3xl md:text-5xl font-bold px-4">
-    Quer aprender a fazer Crochê?
-  </h1>
+        <h1 className="text-[#B27738] text-3xl md:text-5xl font-bold px-4">
+          Quer aprender a fazer Crochê?
+        </h1>
 
-  <p className="max-w-7xl text-lg md:text-2xl text-gray-700 px-4 leading-relaxed">
-    Desperte a sua criatividade e domine a arte do croché com uma formação pensada especialmente para si! 
-    Em apenas duas semanas, vai aprender desde os pontos mais básicos até a criação de peças incríveis, 
-    como a elegante peça square. Além disso, terá acesso a bónus exclusivos que vão ajudar a transformar 
-    o seu talento numa oportunidade de negócio. As vagas são limitadas — não perca a chance de criar, 
-    empreender e dar vida às suas ideias com as próprias mãos!
-  </p>
-</section>
+        <p className="max-w-7xl text-lg md:text-2xl text-gray-700 px-4 leading-relaxed">
+          Desperte a sua criatividade e domine a arte do croché com uma formação pensada especialmente para si! 
+          Em apenas duas semanas, vai aprender desde os pontos mais básicos até a criação de peças incríveis, 
+          como a elegante peça square. Além disso, terá acesso a bónus exclusivos que vão ajudar a transformar 
+          o seu talento numa oportunidade de negócio. As vagas são limitadas — não perca a chance de criar, 
+          empreender e dar vida às suas ideias com as próprias mãos!
+        </p>
+      </section>
 
 
-<section className="bg-gradient-to-b from-[#DFA464] to-[#B27738] flex flex-col justify-center gap-10 py-[8rem]">
-  <h1 className="text-white text-3xl md:text-5xl font-bold px-4 flex justify-center pb-15">Informações do curso</h1>
-  
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-100 px-8 
-                  justify-items-center max-w-7xl mx-auto ">
-    
-    <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
-                    flex flex-col items-center justify-center rounded-lg shadow-lg p-6 mx-[20rem]">
-      <img src={location} alt="location" className="w-20 h-20"/>
-      <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
-        Talatona/Bairro militar de frente ao antigo IFAL
-      </h1>
-    </div>
+    <section className="bg-gradient-to-b from-[#DFA464] to-[#B27738] flex flex-col justify-center gap-10 py-[8rem]">
+      <h1 className="text-white text-3xl md:text-5xl font-bold px-4 flex justify-center pb-15">Informações do curso</h1>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-100 px-8 
+                      justify-items-center max-w-7xl mx-auto ">
+        
+        <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
+                        flex flex-col items-center justify-center rounded-lg shadow-lg p-6 mx-[20rem]">
+          <img src={location} alt="location" className="w-20 h-20"/>
+          <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
+            Talatona/Bairro militar de frente ao antigo IFAL
+          </h1>
+        </div>
 
-    <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
-                    flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
-      <img src={calend} alt="calendar" className="w-20 h-20"/>
-      <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
-        Dia 6 de Setembro
-      </h1>
-    </div>
+        <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
+                        flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
+          <img src={calend} alt="calendar" className="w-20 h-20"/>
+          <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
+            Dia 6 de Setembro
+          </h1>
+        </div>
 
-    <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
-                    flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
-      <img src={clock} alt="clock" className="w-20 h-20"/>
-      <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
-        Segunda - Sexta <br /> 14h - 16h <br /> Sábado: 9h - 12h
-      </h1>
-    </div>
+        <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
+                        flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
+          <img src={clock} alt="clock" className="w-20 h-20"/>
+          <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
+            Segunda - Sexta <br /> 14h - 16h <br /> Sábado: 9h - 12h
+          </h1>
+        </div>
 
-    <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
-                    flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
-      <img src={money} alt="money" className="w-20 h-20"/>
-      <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
-        20 000, 00kz <br /> Material por nossa conta.
-      </h1>
-    </div>
-  </div>
-</section>
+        <div className="bg-amber-50 w-[280px] md:w-[300px] lg:w-[340px] h-[320px] 
+                        flex flex-col items-center justify-center rounded-lg shadow-lg p-6">
+          <img src={money} alt="money" className="w-20 h-20"/>
+          <h1 className="text-[#B27738] text-xl font-semibold text-center mt-4">
+            20 000, 00kz <br /> Material por nossa conta.
+          </h1>
+        </div>
+      </div>
+    </section>
 
 
  
-      <section className="flex justify-around items-center p-20 min-h-[700px] gap-20">
-        <div className="">
-          <h1 className="text-[#B27738] p-8 text-4xl font-bold"> Com a formação vai aprender</h1>
+    <section className="flex justify-around items-center p-20 min-h-[700px] gap-20">
+      <div className="">
+        <h1 className="text-[#B27738] p-8 text-4xl font-bold"> Com a formação vai aprender</h1>
           <p className="text-3xl text-[#a57644]">Pontos De Base:</p>
           <p className="text-2xl text-gray-700"> Baixissimo</p>
           <p className="text-2xl text-gray-700">  Meio alto</p>
@@ -74,13 +75,12 @@ export const Home = () => {
           <p className="text-3xl text-[#a57644]">Projeto final:</p>
           <p className="text-2xl text-gray-700">  Peça square.</p>
           <p className="text-2xl text-gray-700">  Elaboração de projeto de gráficos.</p>
-        </div>
+      </div>
 
-        <div>
-          <p>img</p>
-        </div>
-
-      </section>
+      <div>
+        <img src={VB_croche} alt="vestidoEblusa" />
+      </div>
+    </section>
 
     </>
   );};
